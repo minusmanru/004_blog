355934b3ef5eccccc448fdce3ef437a0acfedd8f
AndreiS <shinkar888@yandex.ru>
2019-11-11 20:10:55 +0300

    004_blog_(add_posts)

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -11,6 +11,38 @@ export class PostsComponent extends Component {
     async onShow() {
         const fbData = await apiService.fetchPost()
         const posts = TransformService.fbObjectToArray(fbData)
-        console.log(posts)
+        // posts.map(post => renderPost(post)) - это массив, поэтому сразу его соеденяем в строку через пробел с помощью .join(' ')
+        const html = posts.map(post => renderPost(post)) 
+        this.$el.insertAdjacentHTML('afterbegin', html.join(' ')) // .join(' ') выводит элементы массива заменяя запятую на пробел
+    }
+
+    onHide() {
+        this.$el.innerHTML = ''
     }
 } 
+
+function renderPost(post) {
+    const tag = post.type === 'news'
+        ? '<li class="tag tag-blue tag-rounded">Новость</li>'
+        : '<li class="tag tag-rounded">Заметка</li>'
+
+    const button = '<button class="button-round button-small button-primary">Сохранить</button>'
+
+   return  `
+        <div class="panel">
+            <div class="panel-head">
+                <p class="panel-title">${post.title}</p>
+                <ul class="tags">
+                ${tag}
+                </ul>
+            </div>
+            <div class="panel-body">
+                <p class="multi-line">${post.fulltext}</p>
+            </div>
+            <div class="panel-footer w-panel-footer">
+                <small>${post.date} </small> 
+                ${button}
+            </div>
+        </div>    
+    `
+}
